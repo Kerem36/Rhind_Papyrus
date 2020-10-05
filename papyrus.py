1edfd59017d439ae88fde8ef2ced1e2bdf4f7536
Kerem36 <55869347+Kerem36@users.noreply.github.com>
2020-10-05 21:11:17 +0300

    Add files via upload

---
+++ papyrus.py
@@ -0,0 +1,33 @@
+def GCD(a, b):
+    for i in range(min(a, b), 0, -1):
+        if a%i == 0 and b%i == 0:
+            return i
+    return 1
+
+
+def main():
+    a = int(input())
+    b = int(input())
+    if b % 2 == 1:
+        a *= 2
+        b *= 2
+    n = 2
+    results = []
+    while a > 1:
+        if 1/n < a/b:
+            new_a = a*n - b
+            new_b = n*b
+            results.append("1/%d"%n)
+            d = GCD(new_a, new_b)
+            a = new_a//d
+            b = new_b//d
+            
+        n += 1
+
+    results.append("1/%d"%b)
+    
+    print(*results, sep="\n")
+
+
+if __name__ == '__main__':
+    main()
